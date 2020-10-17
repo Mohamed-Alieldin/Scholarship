36bb751ea4851cca226549b28a5585d447aa15f5
Alieldin <mohamed.alieldin@nagwa.com>
2020-10-17 21:15:45 +0200

    Admin Page and Actions

---
+++ ScholarshipApp/Models/Admin/ApplicationsListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScholarshipApp.Models.Admin
+{
+    public class ApplicationsListViewModel
+    {
+        public List<ApplicationEntry> Applications { get; set; }
+    }
+
+    public class ApplicationEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string BirthDate { get; set; }
+        public string University { get; set; }
+        public string Major { get; set; }
+        public float GPA { get; set; }
+        public string  ResumeFileName { get; set; }
+
+        public int? IsAccepted { get; set; }
+    }
+}
